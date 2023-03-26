9d5c935c79aa2d27fde676c5cccd780ff7722acb
Evan Martin <evan.martiin29@gmail.com>
2023-03-26 11:59:13 +0200

    feat(guildMemberAdd): change welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,6 +4,6 @@ import { EMOJIS } from "../constants.js";
 export const GuildMemberAdd = {
   name: Events.GuildMemberAdd,
   execute(user) {
-    user.send({ content: `${EMOJIS.WAVE} Bienvenue !\nLe **Country of the Day**, c'est un nouveau pays tous les matins à 10h30.\n\nPour le recevoir, écris \`/subscribe\` dans <#${process.env.DISCORD_GENERAL_CHANNEL_ID}>.\nPour te désinscrire, c'est \`/unsubscribe\`.` })
+    user.send({ content: `${EMOJIS.WAVE} Bienvenue !\nLe **Country of the Day**, c'est un nouveau pays tous les matins à 10h30.\n\nPour le recevoir, écris \`/subscribe\` dans <#${process.env.DISCORD_GENERAL_CHANNEL_ID}>.\nPour te désinscrire, c'est \`/unsubscribe\`.\n\nN'oublie pas que les informations quotidiennes proviennent de ChatGPT, elles ne sont donc pas vérifiées et des erreurs peuvent être publiées.` })
   },
 };
